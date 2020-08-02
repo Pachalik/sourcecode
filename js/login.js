9e73adc7d95ad1fe9c8164ff421eb1784b91f32e
Pachalik <67580517+Pachalik@users.noreply.github.com>
2020-08-02 23:52:16 +0200

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,45 @@
+function customAlert(bgcolor, text){
+    if(bgcolor=="green"){
+        document.getElementById("alert").style = "background-color: #27ae60";
+    }else if(bgcolor=="red"){
+        document.getElementById("alert").style = "background-color: #c0392b";
+    }
+    document.getElementById("alert").innerHTML = text;
+}
+
+function tryLogin(){
+    // ID
+    var allIds = ['pachalik', 'pioupia'];
+    var idTested = document.getElementById('id').value;
+    var passwordTested = document.getElementById('password').value;
+    var recognized = false;
+    var idNumber;
+    var loggedIn = false;
+
+    for(let i = 0; i<allIds.length; i++){
+        if(allIds[i] == idTested.toLowerCase()){
+            recognized = true;
+            idNumber = i;
+        }
+    }
+    
+    if(recognized==true){
+        console.log("ID reconnu");
+    } else if(recognized==false){
+        console.log("ID non reconnu")
+        customAlert('red', 'ID non reconnu');
+    }
+    
+    // Password
+    var allPasswords = ['Le chameau aboie la caravane passe', "Du contrat social"];
+
+    if(passwordTested==allPasswords[idNumber]){
+        console.log("Mot de passe correct")
+        customAlert('green', 'Mot de passe correct')
+        loggedIn = true;
+        console.log('Vous êtes désormais connecté')
+    }else {
+        console.log('Mot de passe incorrect');
+        customAlert('red', 'Mot de passe incorrect');
+    }
+}
